c4c4a356ebf7c654a977da5dcf60eb58eee23cb5
Yuri Silva <79874698+projects-yuri@users.noreply.github.com>
2022-06-06 09:07:10 -0300

    Add files via upload

--- aula_30_05_22/script.js
+++ aula_30_05_22/script.js
@@ -5,6 +5,27 @@ window.onsubmit = (form) =>
     const male = form.target.querySelector('#male').checked
     const female = form.target.querySelector('#female').checked
 
-    console.log(`Year of Birth => ${yearBirth}`)
-    console.log(`Male => ${male}, Female => ${female}`)
+    const ageBirth = (birth) =>
+    {
+        // We can use "Date" from JS to get the fullYear
+        const age = 2022 - birth
+        return age
+    }
+    const stageCheck = (stage) =>
+    {
+        if(stage == 0){return}
+        if(stage <= 10){return 'Child'}
+        if(stage > 10 && stage <= 20){return 'Teen'}
+        if(stage > 20){return 'Adult'}
+        if(stage > 104){return false}
+    }
+    const genderCheck = (gender) =>
+    {
+        if(gender == true){}
+    }
+
+
+    // ate 10 anos = bebe
+    // ate 20 = adolescente
+    // acima de 20 = adulto
 }
